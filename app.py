8e15c160b9f9bb34ccf1b6ca1c075ba60b9a1290
varadahe <hvarada@vertafore.com>
2025-09-25 12:40:33 +0530

    Adding real chat box adding key

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import streamlit as st
 from openai import OpenAI
+from dotenv import load_dotenv
 
 # Load API key
+load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
